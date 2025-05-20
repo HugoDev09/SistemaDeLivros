fa437382c2cda2ee32c2f405eb425dd0b75f20eb
HugoDev09 <hugodev09@gmail.com>
2025-05-20 11:47:32 -0300

    first commit

---
+++ src/com/app/App.java
@@ -0,0 +1,61 @@
+package com.app;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> bookList = new ArrayList<>();
+        Book books = new Book();
+
+        int option;
+
+        System.out.print("\nMenu");
+        System.out.print("\nInserir livro - 1: ");
+        System.out.print("\nPesquisar livro - 2 ");
+        System.out.print("\nDeletar livro - 3");
+        System.out.print("\nOpção: ");
+        option = scanner.nextInt();
+        scanner.nextLine();
+
+        switch (option) {
+            case 1:
+                System.out.println("Livro que seja inserir: ");
+                books.setName(scanner.nextLine());
+                System.out.println("Livro " + books.getName() + " adicionado.");
+                break;
+
+            case 2:
+                System.out.println("Informe o nome do livro: ");
+                books.setName(scanner.nextLine());
+                bookList.add(books.getName());
+
+                for (String book : bookList){
+                    if (book.equals(books.getName())){
+                        System.out.println("Livro " + books.getName() + " econtrado.");
+                        break;
+                    } else {
+                        System.out.println("O livro " + books.getName() + " não existe.");
+                    }
+                }
+                break;
+
+            case 3:
+                System.out.println("Informe o livro: ");
+                books.setName(scanner.nextLine());
+                if (bookList.remove(books.getName())) {
+                    System.out.println("Livro " + books.getName() + " removido.");
+                } else {
+                    System.out.println("O livro" + books.getName() + " não está em estoque.");
+                }
+                break;
+
+            default:
+                System.out.println("Opções inválidas.");
+        }
+        scanner.close();
+    }
+}
